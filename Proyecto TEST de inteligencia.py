ef4bf6107c5707444680fab94ee49b241bdef104
A01704132-Armando <89037376+A01704132-Armando@users.noreply.github.com>
2021-10-06 22:00:04 -0500

    Add files via upload

--- Proyecto TEST de inteligencia.py
+++ Proyecto TEST de inteligencia.py
@@ -14,41 +14,60 @@ def Probabilidad (lista):
     R=((Total)-(Grupo_interes))/(Total)
     return R*100
 
-def Intentos_mate (Res_correcta):
+### Aqui agregue el uso de matrices para calcular el modulo del vector resultante de una suma de vectores 
+def modulo_vector_resultante (matriz):
+    i=0
+    Suma_componentesx=0
+    Suma_componentesy=0
+    for linea in matriz:
+        j=0
+        for columna in linea:
+            if j==0:
+                Suma_componentesx=Suma_componentesx + matriz[i][j]
+            if j==1:
+                Suma_componentesy=Suma_componentesy + matriz[i][j]
+            j=j+1
+        i=i+1
+        
+    Modulo=((Suma_componentesx**2)+(Suma_componentesy**2))
+    Modulo=Modulo**(1/2)
+    return Modulo
+
+
+def Intentos_mate (Res_correcta, cont_mate):
     i=0
     while i<2:
         R=float(input("Introduce tu respuesta"))
         if R ==Res_correcta:
-            i=i+2
-            return print ("Muye bien hecho", nombre, "Tu respuesta es correcta")
-            return print(cont_mate)
+            print ("Muye bien hecho", nombre, "Tu respuesta es correcta")
+            return cont_mate+1
         elif R !=Res_correcta:
-            if i==0:
-                print ("Has fallado, intenta otra vez")
-            else:
-                return print("Se agotaron tus intentos, has fallado :( \nRespuesta correcta:","%.1f" %Res_correcta)
-                return print(cont_mate)
+            print ("Has fallado, intenta otra vez")
         i=i+1
-      
         
-def Intentos_español (Res_correcta):
+    print("Se agotaron tus intentos, has fallado :( \nRespuesta correcta:","%.1f" %Res_correcta)
+    return cont_mate
+        
+
+        
+def Intentos_español (Res_correcta, cont_español):
     i=0
     while i<2:
         R=str(input("Introduce tu respuesta"))
         if R ==Res_correcta:
-            i=i+2
-            return print ("Muye bien hecho", nombre, "Tu respuesta es correcta")
+            print ("Muye bien hecho", nombre, "Tu respuesta es correcta")
+            return cont_español+1
         elif R !=Res_correcta:
-            if i==0:
                 print ("Has fallado, intenta otra vez")
-            else:
-                return print("Se agotaron tus intentos, has fallado :( \nRespuesta correcta:",Res_correcta)
-                 
+            
         i=i+1
-##Solo faltaría agregar contadores para ir sumando la calificación
-##Pero me surgieron nuevas dudas y volveré a checarlo en asesoria con el profesor el lunes
+   
+    print("Se agotaron tus intentos, has fallado :( \nRespuesta correcta:",Res_correcta)
+    return cont_español         
+ 
+ 
 #Inicio del programa
-
+#Ya termine de implementar contadores y el programa ya esta completo
 nombre=input("Introduzca su nombre")
 edad= input("Introduzca su edad")
 print("")
@@ -62,64 +81,78 @@ print("")
 print("")
 print ("Sección matemáticas")
 print("")
-
+cont_mate=0
  
 print ("Redondea tu respuesta a 1 solo decimal en caso de ser necesarios")
 
 print ("Pregunta 1: Calcula el volumen de un cilindro con los siguientes datos: \nPI=3.1415 \nradio=5 \naltura=10")
 Vol= float("%.1f" %Vol_cilindro(3.1415, 5, 10))
-Intentos_mate(Vol)
+cont_mate=Intentos_mate(Vol, cont_mate)
 
 
 print()
 print()
-
-print ("Pregunta 2: Un automóvil inicia su trayecto con una velocidad de 12.3 m/s y termina su trayecto a una velocidad de 150 m/s. \nSi su tiempo en hacer el recorrido fue de 20 segundos, calcula su aceleración promedio")
-Ace=float("%.1f" %Aceleracion(12.3, 150, 20))
-Intentos_mate (Ace)
-
+###Cambie este problema anterior por un probelma de vectores para usar las matrices
+print ("Pregunta 2: El vector R es la suma del vector A (3i + 4j) y el vector B (5i -6j) \n Calcula el módulo o magnitud del vector R")
+Matriz=[[3, 4],[5,-6]]
+Modulo_vector=float("%.1f" %modulo_vector_resultante (Matriz))
+cont_mate=Intentos_mate(Modulo_vector, cont_mate)
 print()
 print()
 
-print ("Pregunta 3 (versión fácil): Si un almacén de petróleo puede contener hasta 100 L \nCalcula al cantidad de petróleo que pueden contener 10 almacenes")
-Regla3=Regla_de_tres(10, 100, 1)
-Intentos_mate (Regla3)
+if cont_mate <2:
+    print ("Pregunta 3 (versión fácil): Si un almacén de petróleo puede contener hasta 100 L \nCalcula al cantidad de petróleo que pueden contener 10 almacenes")
+    
+    Regla3=Regla_de_tres(10, 100, 1)
+    cont_mate=Intentos_mate(Regla3, cont_mate)
 
+else:
+    print ("Pregunta 3 (versión difícil): En una bolsa hay 8 canicas azules, 4 canicas verdes, 1 canica amarilla, 3 canicas naranjas y 7 canicas moradas \nSi agarro una canica al azar, que posibilidad hay de que no me salga una canica amarilla o naranja ")
+    print()
+    print ("Recuerda multiplicar tu resultado x100 para que el resultado de tu porcentaje sea de 0 a 100")
+    lista=[8, 4, 1, 3, 7]
+    Prob=float("%.1f" %Probabilidad(lista))
+    cont_mate=Intentos_mate(Prob, cont_mate)
+  
 
 print()
 print()
-###Añadí el uso de listas en este problema matemático
-print ("Pregunta 3 (versión difícil): En una bolsa hay 8 canicas azules, 4 canicas verdes, 1 canica amarilla, 3 canicas naranjas y 7 canicas moradas \nSi agarro una canica al azar, que posibilidad hay de que no me salga una canica amarilla o naranja ")
-print()
-print ("Recuerda multiplicar tu resultado x100 para que el resultado de tu porcentaje sea de 0 a 100")
-lista=[8, 4, 1, 3, 7]
-Prob=float("%.1f" %Probabilidad(lista))
-Intentos_mate(Prob)
-
-print()
-print()
+print("Tu puntuación de matemáticas ha sido de", cont_mate, "de 3")
 print()
 
 print("Sección español")
 print("Importante: Escribe tus respuestas empezando con mayusculas y con sus acentos correspondientes")
+cont_español=0
 
 print ("Pregunta 1: Cual de las siguientes palabras es esdrújula: \nMaíz \nÁrbol \nSábado")
-Intentos_español ("Sábado")
+cont_español=Intentos_español ("Sábado", cont_español)
 
 print()
 print()
 
 print ("Pregunta 2: En esta oración, ¨Escribiste mal esa palabra, ya que hidrógeno lleva acento en la primera o¨, \ncual de las siguientes funciones del lenguaje predomina: \nMetalinguística \nReferencial \nEmotiva ")
-Intentos_español ("Metalinguística")
+cont_español=Intentos_español ("Metalinguística", cont_español)
 
 print()
 print()
 
-print ("Pregunta 3 (versión fácil): Cuál de las siguientes palabras es sinónimo de intromisión: \nPleito \nCorrespondencia \nIntrusión \nConjetura  ")
-Intentos_español ("Intrusión")
-
+if cont_español<2:
+    print ("Pregunta 3 (versión fácil): Cuál de las siguientes palabras es sinónimo de intromisión: \nPleito \nCorrespondencia \nIntrusión \nConjetura  ")
+    cont_español=Intentos_español ("Intrusión", cont_español)
+    
+else:
+    print ("Pregunta 3 (versión difícil): En la siguiente frase: ¨Lleve a mi novia a Francia y en ese país tan romántico le pedí su mano¨, Escribe la figura retórica empleada")
+    cont_español=Intentos_español ("Sinécdoque",cont_español)
+print()
+print("Tu puntuación de español ha sido de", cont_español, "de 3")
 print()
 print()
+print()
 
-print ("Pregunta 3 (versión difícil): En la siguiente frase: ¨Lleve a mi novia a Francia y en ese país tan romántico le pedí su mano¨, Escribe la figura retórica empleada")
-Intentos_español ("Sinécdoque")
+if cont_mate!=cont_español:
+    if cont_mate<cont_español:
+        print ("Dados los resultados de tu examen", nombre, "es notable que tu inteligencia en español es superior a tu inteligencia en matemáticas")
+    else:
+        print ("Dados los resultados de tu examen", nombre, "es notable que tu inteligencia en matemáticas es superior a tu inteligencia en español")
+else:
+    print ("Dados los resultados de tu examen", nombre, "podemos afirmar que dominas tanto las matemáticas como el español por igual")
